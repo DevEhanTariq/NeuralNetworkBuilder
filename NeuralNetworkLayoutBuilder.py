5b3d787c5a1bdb5949c8527cc359c2d746f0495a
DevET <ehan.tariq.21@icloud.com>
2026-06-30 22:21:07 +0100

    Feat: Added four activation functions
    - Added ReLU, LeakyReLU, PReLU, ELU

--- NeuralNetworkLayoutBuilder.py
+++ NeuralNetworkLayoutBuilder.py
@@ -1,4 +1,47 @@
+import math
+
 from libraries import *
 
+class NNLB: # NeuralNetworkLayoutBuilder
+    def __init__(self):
+        pass
+
+    def InputLayer(self, length): # Creates a layer of Neurons
+        pass
+
+    def Hiddenlayer(self, length): # Creates a layer of Neurons
+        pass
+
+    def OutputLayer(self, length): # Creates a layer of Neurons
+        pass
+
+#   A bunch of different Activation layers
+
+    def ReLU(self, x): # If x is greater than 0, return x. Else, return 0
+        if x > 0:
+            return x
+        else:
+            return 0
+
+    def LeakyReLU(self, x, gradient: float = 0.01): # If x is greater than 0, return x. Else return x times the gradients
+        if x >= 0:
+            return x
+        else:
+            return x*gradient
+
+    def PReLU(self, x, alpha: float = 0):
+        if x >= 0:
+            return x
+        else:
+            return x * alpha
+
+    def ELU(self, x, hyperperameter: float = 1):
+        if x >= 0:
+            return x
+        else:
+            return hyperperameter * (math.exp(x) - 1)
+
+
+
 if __name__ == "__main__":
-    pass
+    NNLB = NNLB()
